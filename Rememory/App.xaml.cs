a58bcbfce426afd0abe347e5db00e216668b484f
hpavlo <67195687+hpavlo@users.noreply.github.com>
2025-01-19 05:31:32 +0200

    migrate main window to separate class

--- Rememory/App.xaml.cs
+++ Rememory/App.xaml.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.Windows.AppNotifications;
 using Microsoft.Windows.AppNotifications.Builder;
 using Microsoft.Windows.Globalization;
 using Rememory.Helper;
-using Rememory.Helper.WindowBackdrop;
 using Rememory.Hooks;
 using Rememory.Models;
 using Rememory.Service;
@@ -16,13 +14,7 @@ using System.Globalization;
 using System.Linq;
 using System.Runtime.InteropServices.Marshalling;
 using System.Threading;
-using Windows.ApplicationModel;
-using Windows.Foundation;
-using Windows.Graphics;
-using Windows.System;
 using WinRT.Interop;
-using WinUIEx;
-using WinUIEx.Messaging;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -38,8 +30,7 @@ namespace Rememory
         /// Gets the current <see cref="App"/> instance in use
         /// </summary>
         public new static App Current => (App)Application.Current;
-        public IntPtr ClipboardWindowHwnd { get; private set; }
-        public Window ClipboardWindow { get; private set; }
+        public IntPtr ClipboardWindowHandle { get; private set; }
         public IThemeService ThemeService { get; private set; }
         public SettingsContext SettingsContext => SettingsContext.Instance;
 
@@ -50,10 +41,9 @@ namespace Rememory
 
         public Microsoft.UI.Dispatching.DispatcherQueue DispatcherQueue { get; private set; } = Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread();
 
+        private ClipboardWindow _clipboardWindow;
         private string[] _launchArguments;
         private IKeyboardMonitor _keyboardMonitor;
-        private WindowMessageMonitor _messageMonitor;
-        private bool _queryEndSessionReceived = false;
 
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
@@ -77,18 +67,17 @@ namespace Rememory
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs args)
         {
-            InitializeMainWindow();
+            _clipboardWindow = new ClipboardWindow();
+            _clipboardWindow.Closed += ClipboardWindow_Closed;
+            ClipboardWindowHandle = WindowNative.GetWindowHandle(_clipboardWindow);
+
+            InitializeRememoryCore();
+            _keyboardMonitor.StartMonitor();
 
             if (_launchArguments.Contains("-settings"))
             {
                 SettingsWindow.ShowSettingsWindow();
             }
-
-            InitializeRememoryCore();
-
-            _messageMonitor = new WindowMessageMonitor(ClipboardWindowHwnd);
-            _messageMonitor.WindowMessageReceived += WindowMessageReceived;
-            _keyboardMonitor.StartMonitor();
 
             if (!_launchArguments.Contains("-silent"))
             {
@@ -100,35 +89,13 @@ namespace Rememory
             }
         }
 
-        private void InitializeMainWindow()
+        public void ShowClipboardWindow() => _clipboardWindow.ShowWindow();
+        public void HideClipboardWindow() => _clipboardWindow.HideWindow();
+
+        private void ClipboardWindow_Closed(object sender, WindowEventArgs args)
         {
-            ClipboardWindow = new WindowEx
-            {
-                Title = Package.Current.DisplayName,
-                IsShownInSwitchers = false,
-                IsAlwaysOnTop = true,
-                IsResizable = false,
-                IsMaximizable = false,
-                IsMinimizable = false,
-                TaskBarIcon = Icon.FromFile(AppContext.BaseDirectory + "Assets\\WindowIcon.ico")
-            };
-            ClipboardWindow.SetWindowStyle(WindowStyle.Popup);
-
-            ClipboardWindowHwnd = WindowNative.GetWindowHandle(ClipboardWindow);
-            int cornerPreference = (int)NativeHelper.DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_ROUND;
-            NativeHelper.DwmSetWindowAttribute(ClipboardWindowHwnd, NativeHelper.DWMWA_WINDOW_CORNER_PREFERENCE, ref cornerPreference, sizeof(int));
-
-            ClipboardWindow.Content = new ClipboardRootPage(ClipboardWindow);
-
-            if (WindowBackdropHelper.IsSystemBackdropSupported)
-            {
-                var backdropHelper = new WindowBackdropHelper(ClipboardWindow);
-                backdropHelper.InitWindowBackdrop();
-            }
-
-            ClipboardWindow.Activated += ClipboardWindow_Activated;
-            ClipboardWindow.AppWindow.Closing += ClipboardWindow_Closing;
-            ClipboardWindow.Closed += ClipboardWindow_Closed;
+            _keyboardMonitor.StopMonitor();
+            Exit();
         }
 
         private void SetCulture(string culture)
@@ -148,8 +115,8 @@ namespace Rememory
 
         private unsafe void InitializeRememoryCore()
         {
-            RememoryCoreHelper.AddWindowProc(ClipboardWindowHwnd);
-            RememoryCoreHelper.CreateTrayIcon(ClipboardWindowHwnd,
+            RememoryCoreHelper.AddWindowProc(ClipboardWindowHandle);
+            RememoryCoreHelper.CreateTrayIcon(ClipboardWindowHandle,
                 new IntPtr(Utf16StringMarshaller.ConvertToUnmanaged(
                     $"{"TrayIconMenu_Open".GetLocalizedResource()}\t{KeyboardHelper.ShortcutToString(SettingsContext.ActivationShortcut, "+")}")),
                 new IntPtr(Utf16StringMarshaller.ConvertToUnmanaged("TrayIconMenu_Settings".GetLocalizedResource())),
@@ -162,115 +129,6 @@ namespace Rememory
                 );
         }
 
-        public bool ShowClipboardWindow()
-        {
-            if (ClipboardWindow.Visible == true)
-                return false;
-            MoveToStartPosition();
-            ClipboardWindow.AppWindow.Show();
-            KeyboardHelper.MultiKeyAction([(VirtualKey)0x0E], KeyboardHelper.KeyAction.DownUp);   // To fix problem with foreground window
-            ClipboardWindow.SetForegroundWindow();
-            return true;
-        }
-
-        public bool HideClipboardWindow()
-        {
-            if (!ClipboardWindow.Visible)
-            {
-                return false;
-            }
-            ClipboardWindow.AppWindow.Hide();
-            return true;
-        }
-
-        public void MoveToStartPosition()
-        {
-            var workArea = GetWorkAreaRectangle();
-
-            int width = SettingsContext.WindowWidth;
-            int margin = SettingsContext.WindowMargin;
-
-            // To update DPI for window
-            ClipboardWindow.AppWindow.Move(new(
-                (int)workArea.Right - width - margin,
-                (int)workArea.Top + margin));
-
-            // Resize window
-            ClipboardWindow.AppWindow.MoveAndResize(new RectInt32(
-                (int)workArea.Right - width - margin,
-                (int)workArea.Top + margin,
-                width,
-                (int)workArea.Height - 2 * margin));
-        }
-
-        private void WindowMessageReceived(object sender, WindowMessageEventArgs args)
-        {
-            switch (args.Message.MessageId)
-            {
-                case NativeHelper.WM_QUERYENDSESSION:
-                    _queryEndSessionReceived = true;
-                    break;
-
-                case NativeHelper.WM_COMMAND:
-                    switch (args.Message.WParam)
-                    {
-                        case RememoryCoreHelper.TRAY_OPEN_COMMAND:
-                            ShowClipboardWindow();
-                            break;
-                        case RememoryCoreHelper.TRAY_SETTINGS_COMMAND:
-                            SettingsWindow.ShowSettingsWindow();
-                            break;
-                        case RememoryCoreHelper.TRAY_EXIT_COMMAND:
-                            ClipboardWindow.Close();
-                            break;
-                    }
-                    break;
-                case RememoryCoreHelper.TRAY_NOTIFICATION:
-                    if (args.Message.LParam == NativeHelper.WM_LBUTTONUP)
-                        ShowClipboardWindow();
-                    break;
-            }
-        }
-
-        private void ClipboardWindow_Activated(object sender, WindowActivatedEventArgs args)
-        {
-            if (args.WindowActivationState == WindowActivationState.Deactivated)
-            {
-                HideClipboardWindow();
-            }
-        }
-
-        private void ClipboardWindow_Closing(AppWindow sender, AppWindowClosingEventArgs args)
-        {
-            if (!_queryEndSessionReceived)
-            {
-                HideClipboardWindow();
-                args.Cancel = true;
-            }
-        }
-
-        private void ClipboardWindow_Closed(object sender, WindowEventArgs args)
-        {
-            SettingsWindow.CloseSettingsWindow();
-            _keyboardMonitor.StopMonitor();
-            ClipboardWindow.Activated -= ClipboardWindow_Activated;
-            _messageMonitor.WindowMessageReceived -= WindowMessageReceived;
-            Exit();
-        }
-
-        private Rect GetWorkAreaRectangle()
-        {
-            NativeHelper.GetCursorPos(out NativeHelper.PointInter point);
-            IntPtr monitor = NativeHelper.MonitorFromPoint(point, NativeHelper.MONITOR_DEFAULTTONEAREST);
-            NativeHelper.MonitorInfoEx info = new();
-            NativeHelper.GetMonitorInfo(monitor, info);
-
-            return new Rect(
-                info.rcWork.left,
-                info.rcWork.top,
-                info.rcWork.right - info.rcWork.left,
-                info.rcWork.bottom - info.rcWork.top);
-        }
         private static IServiceProvider ConfigureServices()
         {
             var services = new ServiceCollection();
