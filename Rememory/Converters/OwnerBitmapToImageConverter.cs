8885152de875b4de4845124df64944e547d61e11
hpavlo <67195687+hpavlo@users.noreply.github.com>
2025-02-14 22:07:44 +0200

    bitmap caching

--- Rememory/Converters/OwnerBitmapToImageConverter.cs
+++ Rememory/Converters/OwnerBitmapToImageConverter.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Media.Imaging;
+using Rememory.Models;
 using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Graphics.Imaging;
 
@@ -8,21 +10,32 @@ namespace Rememory.Converters
 {
     public class OwnerBitmapToImageConverter : IValueConverter
     {
+        // Saves cached bitmaps for each application to make them reusable
+        private static Dictionary<string, SoftwareBitmapSource> _cachedBitmaps = [];
+
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            if (value is not null)
-            {
-                var pixels = (byte[])value;
-                int imageSide = (int)Math.Sqrt(pixels.Length / 4);
+            var item = (ClipboardItem)value;
 
+            if (_cachedBitmaps.TryGetValue(item.OwnerPath, out var cachedBitmap))
+            {
+                return cachedBitmap;
+            }
+
+            if (item.OwnerIconBitmap is not null)
+            {
+                int imageSide = (int)Math.Sqrt(item.OwnerIconBitmap.Length / 4);
                 var straightBitmap = new SoftwareBitmap(BitmapPixelFormat.Bgra8, imageSide, imageSide, BitmapAlphaMode.Straight);
-                straightBitmap.CopyFromBuffer(pixels.AsBuffer());
+                straightBitmap.CopyFromBuffer(item.OwnerIconBitmap.AsBuffer());
                 var premultipliedBitmap = SoftwareBitmap.Convert(straightBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
 
                 var bitmapSource = new SoftwareBitmapSource();
                 bitmapSource.SetBitmapAsync(premultipliedBitmap).AsTask();
+
+                _cachedBitmaps.Add(item.OwnerPath, bitmapSource);
                 return bitmapSource;
             }
+
             return null;
         }
 
