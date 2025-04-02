4de7191100b82e15bd0e58a3056720361a65cb7f
hpavlo <67195687+hpavlo@users.noreply.github.com>
2025-04-03 01:57:00 +0300

    new sqlite service

---
+++ Rememory/Models/NewModels/DataModel.cs
@@ -0,0 +1,15 @@
+﻿using Rememory.Helper;
+
+namespace Rememory.Models.NewModels
+{
+    public class DataModel(ClipboardFormat format, string data, byte[] hash)
+    {
+        public int Id { get; set; }
+
+        public ClipboardFormat Format { get; set; } = format;
+
+        public string Data { get; set; } = data;
+
+        public byte[] Hash { get; set; } = hash;
+    }
+}
