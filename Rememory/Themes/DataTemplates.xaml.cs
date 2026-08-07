0c1c43ae867d7e287bc97e3d515d743b441ef633
hpavlo <67195687+hpavlo@users.noreply.github.com>
2026-08-08 02:38:48 +0300

    Move data templates to a separate resources file

---
+++ Rememory/Themes/DataTemplates.xaml.cs
@@ -0,0 +1,13 @@
+using Microsoft.UI.Xaml;
+
+namespace Rememory.Themes
+{
+    // Using code behind for DataTemplates resources to allow use x:Bind in the DataTemplates.xaml file
+    public sealed partial class DataTemplates : ResourceDictionary
+    {
+        public DataTemplates()
+        {
+            InitializeComponent();
+        }
+    }
+}
